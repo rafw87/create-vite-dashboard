27d38a2b57884f87018aa054806ce468af390866
Rafal Witczak <rafal.witczak87@gmail.com>
2024-08-03 21:47:18 +0200

    Add Prettier & Eslint rules

--- template/.eslintrc.cjs
+++ template/.eslintrc.cjs
@@ -1,18 +1,53 @@
+const path = require('path');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+      alias: {
+        map: [['@', path.resolve(__dirname, './src')]],
+        extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/no-deprecated': 'warn',
+    'import/no-empty-named-blocks': 'error',
+    'import/no-extraneous-dependencies': 'warn',
+    'import/no-amd': 'error',
+    'import/no-commonjs': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-self-import': 'error',
+    'import/no-useless-path-segments': 'warn',
+    'import/extensions': ['warn', 'never'],
+    'import/order': [
       'warn',
-      { allowConstantExport: true },
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      },
     ],
   },
-}
+};
